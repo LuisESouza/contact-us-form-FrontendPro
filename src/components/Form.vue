026998dcf0b0696703da805f05750a097cdefaa7
LuisESouza <leduardosouza2204@gmail.com>
2024-05-01 17:16:16 -0400

    First commit

---
+++ src/components/Form.vue
@@ -0,0 +1,113 @@
+<script setup>
+    import MessageForm from "./MessageForm.vue";
+    import { ref } from "vue";
+
+    const ACCESS_KEY = "543c4711-7969-4d93-9ce3-d582a76282b4";
+    const name = ref("");
+    const email = ref("");
+    const subject = ref("");
+    const message = ref("");
+
+    const submit = async () => {
+        if (name.value.trim() == "" || subject.value.trim() == "" || message.value.trim() == "") {
+            alert("Enter the data correctly");
+            return;
+        }
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                access_key: ACCESS_KEY,
+                name: name,
+                email: email,
+                subject: subject,
+                message: message,
+            }),
+        });
+
+        const result = await response.json();
+        if (result.success) {
+            alert("Email sent successfully!");
+            console.log(result);
+        }
+    };
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <MessageForm />
+        <input v-model="name" type="text" id="name" placeholder="Name">
+        <input v-model="email" type="email" id="email" placeholder="Email">
+        <input v-model="subject" type="text" id="subject" placeholder="Subject">
+        <textarea v-model="message" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+        <button>Send</button>
+    </form>
+</template>
+
+<style scoped>
+    form {
+        background-color: #FFFFFF;
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+        width: 30%;
+        margin: auto;
+        margin-top: 10vh;
+        padding: 25px;
+        border-radius: 6px;
+        align-items: center;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+    }
+    input{
+        padding: 8px;
+        outline: none;
+        border: 1px solid gray;
+        border-radius: 5px;
+        width: 80%;
+    }
+    input:focus{
+        border: 1px solid #7985FF;
+        color: #7985FF;
+    }
+    textarea{
+        resize: none;
+        padding: 10px;
+        outline: none;
+        border: 1px solid gray;
+        border-radius: 5px;
+        width: 80%;
+    }
+    textarea:focus{
+        border: 1px solid #7985FF;
+        color: #7985FF;
+    }
+    button{
+        padding: 8px;
+        height: 43px;
+        width: 85%;
+        background: linear-gradient(to right, #65ADFF, #778AFF);
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        color: white;
+        margin-bottom: 50px;
+    }
+    button:hover{
+        transform: scale(1.02);
+        transition: .4s;
+        border: 2px solid #7985FF;
+        box-shadow: 0 0 15px rgba(101, 173, 255, 0.5);
+    }
+    button:focus{
+        outline: none;
+        transform: scale(1.02);
+        transition: .4s;
+        border: 2px solid #7985FF;
+        box-shadow: 0 0 15px rgba(101, 173, 255, 0.5);
+    }
+
+</style>
---
+++ src/components/MessageForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h3>Send me a message</h3>
+        <p>Feel free to get in touch with me with anything reloted to CODINGSPACE 
+           or you can just say hi.I will get back to you as soon as I can
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        margin-top: 25px;
+    }
+    h3{
+        text-align: center;
+    }
+    p{
+        font-size: 12.5px;
+        line-height: 1.5;
+        text-align: center;
+    }
+</style>
